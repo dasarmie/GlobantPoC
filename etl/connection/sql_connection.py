98f44190ac00e0d3bb43ef844f47ad382248e42f
Asus <dasa1088@gmail.com>
2023-04-05 18:38:50 -0500

    historic data load

--- etl/connection/sql_connection.py
+++ etl/connection/sql_connection.py
@@ -1,15 +1,15 @@
-# Import libraries
 import pyodbc
 
-# set up SQL Server connection
-server = 'sqlglobantpoc.database.windows.net'
-database = 'GlobantPoC'
-username = 'admindb'
-password = 'Medellin01*'
-
-#Create SQL Server connection
-try:
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
-    print("Successful connection")
-except Exception as ex:
-    print(ex)
+class sqlConnection:
+	
+	def __init__(self, server, database):
+		self.conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID=admindb;PWD=Medellin01*')
+		self.cursor = self.conn.cursor()
+		
+	def insert_data_sql(self, table_name, columns, df_name):
+		string_truncate = f"TRUNCATE TABLE {table_name}"
+		self.cursor.execute(string_truncate)
+		string_sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['?']*len(columns))})"
+		for row in df_name.values.tolist():
+			self.cursor.execute(string_sql, row)
+		self.conn.commit()
